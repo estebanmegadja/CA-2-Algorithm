060fa27f1c27451a88c6f33b8df6c9c03142ebf0
Emegadjafalchi-coder <2025367@student.cct.ie>
2026-05-15 14:16:08 +0100

    test

---
+++ CA-2-algorithm/src/main/java/labpro/ca/algorithm/CA2Algorithm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labpro.ca.algorithm;
+
+/**
+ *
+ * @author emega
+ */
+public class CA2Algorithm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
